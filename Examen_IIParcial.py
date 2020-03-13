3d0d00a48cbcebcceb941b7f194ebe126e0684af
LuisBauti <luisbautista551@gmail.com>
2020-03-13 17:13:10 -0600

    Declaracion de la funcionalidad del menu

---
+++ Examen_IIParcial.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Programa: Examen_IIParcial
+# Objetivo: Control de estacionamiento segun el tipo de vehiculo
+# Repositorio: https://github.com/LuisBauti/examen-Multiplataforma/tree/develop
+# Autor: Luis Enrique COnsuegra Bautista
+# Fecha: 13/03/2020
+
+
+from datetime import datetime, timedelta
