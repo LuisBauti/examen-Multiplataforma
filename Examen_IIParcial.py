ee9d5b466925b2b188d18120e4f962f8ac3e55bf
LuisBauti <luisbautista551@gmail.com>
2020-03-13 17:55:28 -0600

    Creacion del menu principal y la funcion para agregar un nuevo vehiculo

--- Examen_IIParcial.py
+++ Examen_IIParcial.py
@@ -5,5 +5,41 @@
 # Autor: Luis Enrique COnsuegra Bautista
 # Fecha: 13/03/2020
 
+import sys
+import os
+import platform
+class automovil(self):
+    """Se encarga de la funcionalidad del ingreso y cobro 
+       del aparcamento"""
 
-from datetime import datetime, timedelta
+    self.vehiculo = list()
+    sel.options = {"1", self.agregar_vehiculo,
+                   "2", self.buscar_vehiculo,
+                   "3",self.reporte,
+                   "4", self.close}
+
+
+
+class Menu(self):
+    """Muestra las opciones para las acciones que se pueden realizar
+       dentro del programa"""
+    
+    print("""
+            Control de Estacionamiento
+            1. Agregar un nuevo vehiculo
+
+            2. Crear tiket
+            3. Ver reporte
+            4. Salir""")
+
+def continuar(self):
+    """Realiza una pausa y solicita una tecla"""
+    imput("\Presione enter para continuar")
+
+def agregar_vehiculo(self)
+    """Agregar un nuevo vehiculo"""
+
+    self.vehiculo.append(
+        imput("Ingrese el tipo de vehiculo")
+    )
+
